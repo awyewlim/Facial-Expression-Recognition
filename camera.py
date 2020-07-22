16e21cb94669bfd5fc8000347ba4cadd8f30b4af
Aw Yew Lim <yewlim00@gmail.com>
2020-07-22 21:29:32 +0800

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,5 +1,5 @@
 import cv2
-from FacialExpressionModel import FacialExpressionModel
+from fem import FacialExpressionModel
 import numpy as np
 
 facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
